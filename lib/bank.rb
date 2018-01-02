bb7947713faf185c73090f9eb18bd7cbeef4d849
cristhiandas <cristhiandf94@gmail.com>
2018-01-02 11:04:42 +0000

    the bank can make deposits calling the transactions

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,3 +1,10 @@
 class Bank
-  
+  def initialize
+    @transaction = Transactions.new
+  end
+
+  def make_deposit(value)
+    @transaction.deposit(value)
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -5,7 +5,8 @@ describe Bank do
   context 'when using transactions' do
 
     it 'Can make deposits' do
+      expect(subject.make_deposit(500)).to be_a(Hash)
     end
-    
+
   end
 end
