5ba0108410cc78683e5b9daddd883f7efabb2fdd
cristhiandas <cristhiandf94@gmail.com>
2018-01-02 10:45:07 +0000

    created a balance variable on the transactions class

--- lib/transactions.rb
+++ lib/transactions.rb
@@ -1,7 +1,14 @@
 class Transactions
 
-  def deposit(value)
-    {transaction: "deposit", value: value}
+  attr_reader :balance
+
+  def initialize
+    @balance = 0
   end
-  
+
+  def deposit(value)
+    @balance = value
+    {transaction: "deposit", value: value, balance: value}
+  end
+
 end
--- spec/transactions_spec.rb
+++ spec/transactions_spec.rb
@@ -4,7 +4,14 @@ describe Transactions do
 
   context 'when making a deposit' do
     it 'saves the value on a hash' do
-      expect(subject.deposit(500)).to eq({transaction: "deposit", value: 500})
+      expect(subject.deposit(500)).to eq({transaction: "deposit", value: 500, balance: 500})
+    end
+
+    it 'changes the balance' do
+      subject.deposit(500)
+      expect(subject.balance).to eq(500)
     end
   end
+
+
 end
