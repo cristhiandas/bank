256564d4af3b4672745772e6099f4a6ba6a66d00
cristhiandas <cristhiandf94@gmail.com>
2018-01-02 10:30:00 +0000

    created an empty transaction class

---
+++ spec/transactions_spec.rb
@@ -0,0 +1,10 @@
+require 'transactions.rb'
+
+describe Transactions do
+
+  context 'when making a deposit' do
+    it 'saves the value on a hash' do
+      # expect(subject.deposit(500)).to eq({ transaction: deposit, value: 500 })
+    end
+  end
+end
