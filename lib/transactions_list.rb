8e4f75ca9990c54da0dfbcc4d7d9b2daaeea67e3
cristhiandas <cristhiandf94@gmail.com>
2018-01-02 11:41:26 +0000

    now it saves deposits on the transaction list

--- lib/transactions_list.rb
+++ lib/transactions_list.rb
@@ -1,2 +1,11 @@
 class TransactionsList
+  attr_reader :transactions
+  
+  def initialize
+    @transactions = []
+  end
+
+  def save_transaction(hash)
+    @transactions << hash
+  end
 end
--- spec/transactions_list_spec.rb
+++ spec/transactions_list_spec.rb
@@ -1,5 +1,13 @@
 require 'transactions_list.rb'
 
 describe TransactionsList do
-
+  context 'when passing a deposit' do
+    it 'can save the transaction' do
+      expect(subject.save_transaction({value: 'something'})).to eq([{value: 'something'}])
+    end
+    it 'has an array of the transaction' do
+      subject.save_transaction({value: 'something'})
+      expect(subject.transactions).to eq([{value: 'something'}])
+    end
+  end
 end
